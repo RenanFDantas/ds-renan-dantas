a0e3d2f932be348c869bfa4291adbbb5dc366fbd
Renan Dantas <renan.dantas3@etec.sp.gov.br>
2024-03-18 09:18:35 -0300

    Aula 2 sobre input e condições

---
+++ aula02/If.java
@@ -0,0 +1,14 @@
+class If {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 50;
+
+        if (x>y) {
+            System.out.println("x e maior que y");
+        } else if (x == y){
+            System.out.println("x e y sao iguais");
+        } else {
+            System.out.println("y e maior que x");
+        }
+   }
+}
